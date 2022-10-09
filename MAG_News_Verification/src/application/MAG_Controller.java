27294df985d452cbabfc478d697873751581fc42
marcociano <m.ciano4@studenti.unisa.it>
2022-10-09 17:51:34 +0200

    First commit

---
+++ MAG_News_Verification/src/application/MAG_Controller.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Worker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author KENSOFT
+ */
+public class MAG_Controller implements Initializable {
+
+    @FXML
+    private TextField txtFieldUrl;
+    @FXML
+    private ProgressBar progress;
+    @FXML
+    private WebView webview;
+    private WebEngine engine;
+    private WebHistory history;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	
+    }
+
+    private void loadUrl() {
+    	engine = webview.getEngine();
+        engine.load("https://" + txtFieldUrl.getText());
+
+        progress.progressProperty().bind(engine.getLoadWorker().progressProperty());
+
+        engine.getLoadWorker().stateProperty().addListener((obs, oldValue, newValue) -> {
+            if (newValue == Worker.State.SUCCEEDED) {
+                System.out.println("Pagina caricata con successo");
+
+                history = webview.getEngine().getHistory();
+                ObservableList<WebHistory.Entry> entries = history.getEntries();
+                txtFieldUrl.setText(entries.get(history.getCurrentIndex()).getUrl());
+                
+                Stage stage = (Stage) txtFieldUrl.getScene().getWindow();
+                stage.setTitle(entries.get(history.getCurrentIndex()).getTitle());
+
+            } else if (newValue == Worker.State.FAILED) {
+                System.out.println("Caricamento fallito");
+            }
+        });
+    }
+
+    @FXML
+    private void back(ActionEvent event) {
+        history = webview.getEngine().getHistory();
+        history.go(-1);
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        txtFieldUrl.setText(entries.get(history.getCurrentIndex()).getUrl());
+    }
+
+    @FXML
+    private void reload(ActionEvent event) {
+        engine.reload();
+    }
+
+    @FXML
+    private void forward(ActionEvent event) {
+        history = webview.getEngine().getHistory();
+        history.go(1);
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+        txtFieldUrl.setText(entries.get(history.getCurrentIndex()).getUrl());
+    }
+
+    @FXML
+    private void txtEnterKey(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            loadUrl();
+        }
+    }
+
+    @FXML
+    private void zoomIn(ActionEvent event) {
+        webview.setZoom(webview.getZoom() + 0.10);
+    }
+
+    @FXML
+    private void zoomOut(ActionEvent event) {
+        webview.setZoom(webview.getZoom() - 0.10);
+    }
+    
+}
---
+++ MAG_News_Verification/src/application/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author KENSOFT
+ */
+public class Main extends Application {
+    
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Scene.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("MAG_News_Verification");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
