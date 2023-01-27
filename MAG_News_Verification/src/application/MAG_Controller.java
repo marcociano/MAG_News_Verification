ecf7a453fc81afbdb85341cb6fc5aa066ab25226
AngelaDeM <angelademartino2000@gmail.com>
2023-01-27 10:52:48 +0100

    Changes :)

--- MAG_News_Verification/src/application/MAG_Controller.java
+++ MAG_News_Verification/src/application/MAG_Controller.java
@@ -145,12 +145,12 @@ public class MAG_Controller implements Initializable {
     	stage.show();
     }
     @FXML
-    private void viewReport(ActionEvent event) throws IOException{
-    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/ReportView.fxml"));
+    private void viewSummary(ActionEvent event) throws IOException{
+    	FXMLLoader loader = new FXMLLoader(getClass().getResource("/SummaryView.fxml"));
     	Parent root= loader.load();
     	Stage stage= new Stage();
     	stage.setScene(new Scene(root));
-    	stage.setTitle("Report Page");
+    	stage.setTitle("Summary Page");
     	stage.setResizable(false);
     	stage.show();
     }
---
+++ MAG_News_Verification/src/application/Summary_Controller.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.ProgressBar;
+
+public class Summary_Controller {
+
+	@FXML
+    private ProgressBar progress;
+		
+	
+}
