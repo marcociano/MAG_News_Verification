8b9ba3f0d657e3396da6db2a2374e6c250425620
marcociano <marco.ciano@live.it>
2023-01-29 19:48:51 +0100

    Bug fixed

--- MAG_News_Verification/src/application/MAG_Controller.java
+++ MAG_News_Verification/src/application/MAG_Controller.java
@@ -146,7 +146,7 @@ public class MAG_Controller implements Initializable {
     @FXML
     private void viewSummary(ActionEvent event) throws IOException{
     	FXMLLoader loader = new FXMLLoader(getClass().getResource("/SummaryView.fxml"));
-        engine.setUserStyleSheetLocation(getClass().getResource("/stylesheet/highlighted_text.css").toString());  
+        engine.setUserStyleSheetLocation(getClass().getResource("/stylesheet/highlighted_text_FakeNews.css").toString());  
     	Parent root= loader.load();
     	Stage stage= new Stage();
     	stage.setScene(new Scene(root));
