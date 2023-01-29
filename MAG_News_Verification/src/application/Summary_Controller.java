68b1f61ddb0798d3606680da59a55802fb1e7d2b
AngelaDeM <angelademartino2000@gmail.com>
2023-01-29 21:44:56 +0100

    Insert alert capable of warning of dangerous news

--- MAG_News_Verification/src/application/Summary_Controller.java
+++ MAG_News_Verification/src/application/Summary_Controller.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Optional;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.ProgressBar;
 
 public class Summary_Controller {
@@ -9,4 +12,19 @@ public class Summary_Controller {
     private ProgressBar progress;
 
 	
+
+	public void reportPage(){
+					
+			ChoiceDialog<String> dialog= new ChoiceDialog<>("Phishing","Malware","Other types of hazards");
+			dialog.setTitle("Report page");
+			dialog.setHeaderText("Be careful, choose well");
+			dialog.setContentText("Choose the type of signaling");
+			
+			Optional<String> result=dialog.showAndWait();
+			
+			if(result.isPresent()) {
+				System.out.println("Your choice: "+result.get());
+			}
+		}
+
 }
