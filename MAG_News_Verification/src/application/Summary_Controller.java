7827c1259b1a55d29709883da189e0d354252993
AngelaDeM <angelademartino2000@gmail.com>
2023-02-04 17:08:04 +0100

    Improvements

--- MAG_News_Verification/src/application/Summary_Controller.java
+++ MAG_News_Verification/src/application/Summary_Controller.java
@@ -1,10 +1,13 @@
 package application;
 
+import static com.itextpdf.text.Element.ALIGN_CENTER;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
@@ -36,14 +39,17 @@ public class Summary_Controller {
 	
 	
 	
-	private int variable=0; 
+	private String variable="Fake"; 
+	
+	private double percentage=76.00;
 	
 	
 	/*Destination Path to save the pdf*/
 	
 	public static final String DEST = "./reportSummary.pdf";
-    public static final String WALLPAPER = "./src/images/wallpaper1.png";
-    public static final String LOGO = "./src/images/MAG_News_Verification_logo.png";
+    public static final String WALLPAPER = "./src/images/wallpaper4.jpg";
+    public static final String LOGODIPARTIMENTO = "./src/images/logoDipartimento.png";
+    public static final String LOGOMAG="./src/images/MAG_News_Verification_logo.png";
 	
 	public void reportPage(){
 					
@@ -110,37 +116,115 @@ public class Summary_Controller {
         /*Here I insert the logo*/
         
         
-        Image img=Image.getInstance(LOGO);
-        img.setAbsolutePosition(0, 0);
+        Image img=Image.getInstance(LOGODIPARTIMENTO);
+        img.setAbsolutePosition(10, 10);
         img.scaleAbsolute(100, 100);
         
         document.add(img);
        
+        /*Managing the text to be displayed in the pdf*/
         
-       
-        Font f=new Font(FontFamily.TIMES_ROMAN,50.0f, Font.UNDERLINE, BaseColor.WHITE);
-        Paragraph p = new Paragraph("MAG News Verification report!", f);
-        document.add(p);
+        /*Management of the first paragraph*/
+        
+        document.add( Chunk.NEWLINE );
+        
+        Font font1 = new Font(FontFamily.TIMES_ROMAN, 35.0f, Font.BOLD, BaseColor.BLACK);
+        
+        Chunk c1 = new Chunk("MAG News Verification report!", font1);
+        
+        /*c1.setBackground(BaseColor.BLUE);*/
+        
+        Paragraph para1 = new Paragraph(c1);
+        
+        para1.setAlignment(ALIGN_CENTER);
+        
+        document.add(para1);
+        
+        document.add( Chunk.NEWLINE );
+        
+        /*MAG logo insertion management*/
+    
+        Image imgs=Image.getInstance(LOGOMAG);
+        imgs.setAbsolutePosition(75, 350);/*width-height*/
+        imgs.scaleAbsolute(400, 400);
+        
+        document.add(imgs);
        
         
-        Font f2=new Font(FontFamily.TIMES_ROMAN, 50.0f, Font.UNDERLINE, BaseColor.WHITE);
-        Paragraph p2 = new Paragraph("The analyzed page contains:" +variable+ "",f);
-        document.add(p2);
+        /*Management of the second paragraph*/
+        
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
        
+        Font font2 = new Font(FontFamily.TIMES_ROMAN, 30.0f, Font.BOLD, BaseColor.BLACK);
+        
+        Chunk c2 = new Chunk("The page analyzed is: " + variable, font2);
+        
+        /*c2.setBackground(BaseColor.BLUE);*/
+        
+        Paragraph para2 = new Paragraph(c2);
+        
+        para2.setAlignment(ALIGN_CENTER);
+        
+        document.add(para2);
+        
+        
+        /*Management of the third paragraph*/
+        
+        document.add( Chunk.NEWLINE );
+        
+        Font font3 = new Font(FontFamily.TIMES_ROMAN, 30.0f, Font.BOLD, BaseColor.BLACK);
+        
+        Chunk c3 = new Chunk("The percentage found is: " + percentage, font3);
+        
+        /*c3.setBackground(BaseColor.BLUE);*/
+        
+        Paragraph para3 = new Paragraph(c3);
+        
+        para3.setAlignment(ALIGN_CENTER);
+        
+        document.add(para3);
+        
+        
+        
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        document.add( Chunk.NEWLINE );
+        
+        
+        /*Management of the fourth paragraph*/
+        
+        Font font4 = new Font(FontFamily.TIMES_ROMAN, 10.0f, Font.BOLD, BaseColor.BLACK);
+        
+        Chunk c4 = new Chunk("Università degli studi di Salerno, Via Papa Giovanni Paolo II, 132, 84084 Fisciano SA" , font4);
+        
+        
+        Paragraph para4 = new Paragraph(c4);
+        
+        para4.setAlignment(ALIGN_CENTER);
+        
+        document.add(para4);
+        
         
         document.close();
+        
         
         /*Once the pdf is saved you will see the message that it was saved*/
         
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information Dialog");
-        alert.setContentText("We would like to inform you that the file was successfully saved");
+        alert.setContentText("We would like to inform you that the file was successfully saved.");
         alert.showAndWait();
         
         
         /*Popup message to indicate Notice that the pdf has been downloaded*/
 		
-		String title= "The pdf has been downloaded";
+		String title= "The pdf has been downloaded.";
     	TrayNotification tray = new TrayNotification();
     	AnimationType type= AnimationType.POPUP;
     	tray.setAnimationType(type);
